95e788205b93c4d5acf5dd27034ff412de093f0e
Jordan Koehler <jordan@Jordans-MacBook-Air.local>
2020-08-02 13:22:11 -0400

    Can now play rock paper scisssors successfully with score increasing

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
-const playerChoice = "";
-const computerChoice = "";
-const score = "";
+var playerChoice = [];
+var computerChoice = [];
+var score = 0;
 const rockPaperScissors = ["rock", "paper", "scissors"];
 
 
 function computerPicks() {
   var randomNumber = Math.floor(Math.random() * 3);
-  var computerChoice = rockPaperScissors[randomNumber];
+  computerChoice.push(rockPaperScissors[randomNumber]);
   console.log(computerChoice);
+}
+
+function playAgain(){
+  playerChoice = [];
+  computerChoice = [];
+  computerPicks();
 }
 
 computerPicks();
 
 $(".icon").click(function() {
   var playerPicks = $(this).attr("id");
-  var playerChoice = playerPicks
+  playerChoice.push(playerPicks);
   console.log(playerChoice);
   winLose();
+  playAgain();
 });
 
 function winLose() {
-  if (playerChoice == computerChoice) {
+  if (playerChoice[0] == computerChoice[0]) {
     console.log('draw');
   }
   else if(playerChoice == "rock") {
     if (computerChoice == "scissors") {
       console.log("win");
+      score++;
+      $("#score-points").text(score);
+      console.log(score);
     } else {
       console.log("lose");
     }
@@ -33,6 +43,9 @@ function winLose() {
   else if(playerChoice == "scissors") {
     if (computerChoice == "paper") {
       console.log("win");
+      score++;
+      $("#score-points").text(score);
+      console.log(score);
     } else {
       console.log("lose");
     }
@@ -40,6 +53,9 @@ function winLose() {
     else if(playerChoice == "paper") {
       if (computerChoice == "rock") {
         console.log("win");
+        score++;
+        $("#score-points").text(score);
+        console.log(score);
       } else {
         console.log("lose");
       }
