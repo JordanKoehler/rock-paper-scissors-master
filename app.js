48b0b06bc2465003a962e497c02781284a4cec22
Jordan Koehler <jordan@Jordans-MacBook-Air.local>
2020-08-02 20:12:24 -0400

    play again restarts the screen but keeps score players choice and computer choice appear

--- app.js
+++ app.js
@@ -10,27 +10,29 @@ function computerPicks() {
   console.log(computerChoice);
 }
 
-function playAgain(){
+function playAgain() {
   playerChoice = [];
   computerChoice = [];
-  computerPicks();
 }
-
-computerPicks();
 
 $(".icon").click(function() {
   var playerPicks = $(this).attr("id");
   playerChoice.push(playerPicks);
   console.log(playerChoice);
-  winLose();
-  playAgain();
+  computerPicks();
+  document.getElementById('RPS-main').style.display = 'none';
+  document.getElementById('versusScreen').style.display = 'block';
+  document.getElementById(playerPicks + "Choice").style.display = "block";
+  setTimeout(function() {
+    document.getElementById(computerChoice + "Computer").style.display = "block"
+    winLose();
+  }, 1000);
 });
 
 function winLose() {
   if (playerChoice[0] == computerChoice[0]) {
     console.log('draw');
-  }
-  else if(playerChoice == "rock") {
+  } else if (playerChoice == "rock") {
     if (computerChoice == "scissors") {
       console.log("win");
       score++;
@@ -39,8 +41,7 @@ function winLose() {
     } else {
       console.log("lose");
     }
-  }
-  else if(playerChoice == "scissors") {
+  } else if (playerChoice == "scissors") {
     if (computerChoice == "paper") {
       console.log("win");
       score++;
@@ -49,23 +50,30 @@ function winLose() {
     } else {
       console.log("lose");
     }
-  }
-    else if(playerChoice == "paper") {
-      if (computerChoice == "rock") {
-        console.log("win");
-        score++;
-        $("#score-points").text(score);
-        console.log(score);
-      } else {
-        console.log("lose");
-      }
+  } else if (playerChoice == "paper") {
+    if (computerChoice == "rock") {
+      console.log("win");
+      score++;
+      $("#score-points").text(score);
+      console.log(score);
+    } else {
+      console.log("lose");
     }
   }
+}
 
-  $(".rules").click(function() {
-  document.getElementById('ruleImage').style.display='block';
-  });
+$(".rules").click(function() {
+  document.getElementById('ruleImage').style.display = 'block';
+});
 
 $(".closeButton").click(function() {
-document.getElementById('ruleImage').style.display='none';
+  document.getElementById('ruleImage').style.display = 'none';
 });
+
+$(".playAgain").click(function() {
+  document.getElementById('RPS-main').style.display = 'block';
+  document.getElementById('versusScreen').style.display = 'none';
+  document.getElementById(playerChoice +"Choice").style.display = "none";
+  document.getElementById(computerChoice + "Computer").style.display = "none"
+  playAgain();
+})
